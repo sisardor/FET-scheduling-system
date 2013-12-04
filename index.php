1e14073ba432ee2124b7798b5726bfbe0c159ed6
Sardor Isakov <sisardor@gmail.com>
2013-12-04 02:33:37 -0800

    Refactoring

--- index.php
+++ index.php
@@ -1,16 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>FET Login</title>
+		<link rel="stylesheet" type="text/css" href="Style/formStyle.css">
+	</head>
+<body>
+	<div id="wrapper">
+		<header>
+			<h1 class="mainheader">FET Login</h1><!--end mainheader-->
+		</header>
+		<form name="login" action="" method="post">
+		<fieldset style="width: 275px; height: 275px; margin: 0 auto;">
+			<legend>Login</legend>
+			<label>User ID:</label><input type="text" name="id" /><br />
+			<label>Password:</label><input type="password" name="pwd" /><br />
+			<input class="buttons" type="submit" value="Login" name="login" /><br />
+			<input class="buttons" type="submit" value="SignUp" name="signup"/>
+		</fieldset>
+		</form>
+	</div>
+</body>
+</html>
 
 <?php
 	include 'PHP/db_connect.php';
 	if(isset($_REQUEST['login'])){
 		$id = $_POST['id'];
 		$pwd = $_POST['pwd'];
+
 		
 		$sql = "SELECT * FROM `$users` WHERE `user_name`='$id' and `password` = '$pwd';";
 		$result = $mysqli->query($sql);
 		$count = $result->num_rows;
+
 		if ($count==1) {
 			session_start();
-			$_SESSION['user']['username']= $id; 
+			$_SESSION['user']['username'] = $id; 
+
 			header("Location: timetables.php");
 			exit;
 		}
@@ -28,95 +54,3 @@
 		exit;
 	}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
-
-    <title>Signin Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="http://localhost2/tmp/FET-scheduling-system/dist/css/bootstrap.css" rel="stylesheet">
-
-
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <style>
- body {
-  padding-top: 40px;
-  padding-bottom: 40px;
-  background-color: #eee;
-}
-
-.form-signin {
-  max-width: 330px;
-  padding: 15px;
-  margin: 0 auto;
-}
-.form-signin .form-signin-heading,
-.form-signin .checkbox {
-  margin-bottom: 10px;
-}
-.form-signin .checkbox {
-  font-weight: normal;
-}
-.form-signin .form-control {
-  position: relative;
-  font-size: 16px;
-  height: auto;
-  padding: 10px;
-  -webkit-box-sizing: border-box;
-     -moz-box-sizing: border-box;
-          box-sizing: border-box;
-}
-.form-signin .form-control:focus {
-  z-index: 2;
-}
-.form-signin input[type="text"] {
-  margin-bottom: -1px;
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
-}
-.form-signin input[type="password"] {
-  margin-bottom: 10px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-}
-  </style>
-
-  <body>
-
-    <div class="container">
-
-      <form name="login" action="" method="post" class="form-signin">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <input type="text" class="form-control" name="id" placeholder="Username"  autofocus>
-        <input type="password" class="form-control" name="pwd" placeholder="Password" >
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit" value="Login" name="login">Sign in</button>
-        <button class="btn btn-lg btn-primary btn-block" type="submit" value="SignUp" name="signup">Sign up</button>
-      </form>
-
-    </div> <!-- /container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-  </body>
-</html>
-
--- timetables.php
+++ timetables.php
@@ -10,106 +10,65 @@
 	$result->free();
 ?>
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+<html>
+<head>
+<meta charset="UTF-8">
+	<title>Time Tables</title>
+	<link rel="stylesheet" type="text/css" href="Style/formStyle.css">
+	<style>
+		 label{margin-top: 0.5em; margin-right: 0.25em;}
+	</style>
+</head>
 
-    <title>Navbar Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="http://localhost2/tmp/FET-scheduling-system/dist/css/bootstrap.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-    <style>
-    body {
-  		padding-top: 20px;
-  		padding-bottom: 20px;
-	}
-
-	.navbar {
-	  margin-bottom: 20px;
-	}
-    </style>
-  </head>
-
-  <body>
-
-    <div class="container">
-
-      <!-- Static navbar -->
-      <div class="navbar navbar-default" role="navigation">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li class="active"><a href="./">Default</a></li>
-            <li><a href="../navbar-static-top/">Static top</a></li>
-            <li><a href="../navbar-fixed-top/">Fixed top</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-
-      <!-- Main component for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <h1>Navbar example</h1>
-        <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-        <p>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
-        </p>
-      </div>
-
-    </div> <!-- /container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-  </body>
+<body>
+	<div id="wrapper">
+		<header>
+			<h1 class="mainheader">FET Time Tables</h1><!--end mainheader-->
+		</header>
+		<article>
+			<form name="fet" action="" method="POST" enctype="multipart/form-data">
+			<fieldset>
+				<legend>Time Tables</legend>
+				
+				<section id="create">
+					<h1>Create a New Table</h1>
+					<label>Semester:</label><input class="text" type="text" name="createSem" /><br/>
+					<label>Institution Name:</label><input style="margin-top: 1.75em; margin-bottom: 1em;" type="text" name="institution" /><br/>
+					<label style="margin-top:0;">Comments:</label><textarea rows="3" cols="25" name="comments"></textarea><br/>
+					<!--This button should add a new entry to user_tables-->
+					<input class="buttons" type="submit" name="create" value="Create Table"/>
+				</section><!--end create-->
+				
+				<section id="exist">
+					<h1>Work on Existing Tables</h1>
+					<label>Semester:</label><select style="margin-bottom: 3em; margin-top: 0.5em;" name="existSem">
+						<option value="" selected></option>
+						<?php echo $semester?>
+					</select><br/>
+					<input class="buttons" type="submit" name="view" value="View/Edit Table"/>
+				</section><!--end exist-->
+				
+				<section id="upload">
+					<h1>Upload a .fet XML File</h1>
+					<label>FET file:</label><input type="file" name="user-file" /><br/>
+					<label>Semester:</label><input class="text" type="text" name="uploadSem" /><br/>
+					<input class="buttons" type="submit" name="upload" value="Upload File" />
+				</section><!--end upload-->
+				
+				<section id="logout">
+					<input class="buttons" type="submit" name="logout" value="Logout" />
+				</section><!--end logout-->
+			</fieldset>
+			</form>
+		</article>
+	</div><!--end wrapper-->
+</body>
 </html>
-
 
 <?php
 
 	$userID = $_SESSION['user']['username'];
-	$pathToMyDir = "/home/mctom03/public_html/Project/uploads/".$userID."/";
+	$pathToMyDir = "/Applications/MAMP/htdocs/tmp/FET-scheduling-system/uploads/".$userID."/";
 
 	#user file upload
 	if(isset($_REQUEST['upload']) && $_REQUEST['uploadSem'] !== NULL){ 
